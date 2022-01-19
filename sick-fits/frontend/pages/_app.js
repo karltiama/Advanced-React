1337c71c39d2c4ac87d0f803f03f46a4f8e53c3d
Karl Tiama <karltiama@gmail.com>
2022-01-19 09:31:31 -0500

    Visualize Route Changes with NProgress

--- sick-fits/frontend/pages/_app.js
+++ sick-fits/frontend/pages/_app.js
@@ -1,7 +1,15 @@
 import Page from "../components/Page"
+import NProgress from 'nprogress';
+import Router from 'next/router';
 
 // TODO: Swap with our own
-import 'nprogress/nprogress.css'
+
+import '../components/styles/nprogress.css'
+
+Router.events.on('routeChangeStart', () => NProgress.start());
+Router.events.on('routeChangeComplete', () => NProgress.done());
+Router.events.on('routeChangeError', () => NProgress.done());
+
 
 export default function MyApp({ Component, pageProps}) {
     return (
