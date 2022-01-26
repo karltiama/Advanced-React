dace926c0fabb607482adba348b1601ccd774929
Karl Tiama <karltiama@gmail.com>
2022-01-26 16:09:01 -0500

    Create auth and add session config

--- sick-fits/backend/keystone.ts
+++ sick-fits/backend/keystone.ts
@@ -1,5 +1,11 @@
-import 'dotenv/config';
+import { createAuth } from '@keystone-next/auth';
 import { config, createSchema } from '@keystone-next/keystone/schema';
+import {
+  withItemData,
+  statelessSessions,
+} from '@keystone-next/keystone/session';
+import { User } from './schemas/Users';
+import 'dotenv/config';
 
 const databaseURL =
   process.env.DATABASE_URL ||
@@ -10,26 +16,50 @@ const sessionConfig = {
   secret: process.env.COOKIE_SECRET,
 };
 
-export default config({
-  server: {
-    cors: {
-      origin: [process.env.FRONTEND_URL],
-      // pass along cookie
-      credentials: true,
-    },
+const { withAuth } = createAuth({
+  listKey: 'User',
+  identityField: 'email',
+  secretField: 'password',
+  initFirstItem: {
+    fields: ['name', 'email', 'password'],
+    // TODO: ADD in inital roles here
   },
-  db: {
-    // db config
-    adapter: 'mongoose',
-    url: databaseURL,
-    // TODO: add data
-  },
-  lists: createSchema({
-    // Schema items go in here
-  }),
-  ui: {
-    // TODO: change this for roles
-    isAccessAllowed: () => true,
-  },
-  // TODO: ADD session values here
 });
+
+export default withAuth(
+  config({
+    server: {
+      cors: {
+        origin: [process.env.FRONTEND_URL],
+        // pass along cookie
+        credentials: true,
+      },
+    },
+    db: {
+      // db config
+      adapter: 'mongoose',
+      url: databaseURL,
+      // TODO: add data
+    },
+    lists: createSchema({
+      // Schema items go in here
+      // if property name and name of
+      // variable you are setting it too
+      // then you can just do this other
+      // wise User: User works too
+      User,
+    }),
+    ui: {
+      // TODO: change this for roles
+      // Show UI only for people who pass this test
+      isAccessAllowed: ({ session }) =>
+        // console.log(session);
+        !!session?.data,
+    },
+    // TODO: ADD session values here
+    session: withItemData(statelessSessions(sessionConfig), {
+      // GraphQL Query
+      User: 'id name email',
+    }),
+  })
+);
